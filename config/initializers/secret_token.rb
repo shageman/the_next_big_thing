dcc5a1b0e1b5bae78775a72f87668a46d1af903d
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-09-11 18:01:40 -0600

    Initial commit: empty "the next big thing" Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheNextBigThing::Application.config.secret_token = 'f0a8e268c6f152a6a8a4833925366d511d62b85fa34098ed120238540e068c9578636785b920537a10c84bc0e512a6f6e0e782e6852971cabeacf96019a85c71'
