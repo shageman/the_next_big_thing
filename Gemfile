84e2a8533c0c1889d683dac209066f1d8d2a51f8
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-09-11 18:18:48 -0600

    Added spec.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails'
 
+group :test, :development do
+  gem 'rspec-rails'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.order = "random"
+end
