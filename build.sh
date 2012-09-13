6d7625938f292fd7dd9873ae172a1f42244cb8df
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-09-12 19:47:34 -0600

    Simplified build script

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 echo "*** Running container app specs"
 source ~/.rvm/scripts/rvm
-source .rvmrc
 bundle exec rake
 result=$?
 
 echo "*** Running teaser engine specs"
 cd engines/teaser
-source ~/.rvm/scripts/rvm
-source .rvmrc
-
 bundle exec rake
 result+=$?
 
