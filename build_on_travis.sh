92c68086b98297382a91041c322539e7d991fee0
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-09-17 20:00:26 -0600

    Bringing travis-ci build script up to date

--- build_on_travis.sh
+++ build_on_travis.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 echo "*** Running container app specs"
-bundle exec rake db:migrate db:test:prepare rspec
+bundle exec rake db:migrate db:test:prepare
+rspec spec
 result=$?
 
-echo "*** Running teaser engine specs"
+
 cd engines/teaser
+echo "*** Running teaser engine specs"
 source "$HOME/.rvm/scripts/rvm"
 bundle install
-bundle exec rake db:migrate app:db:test:prepare rspec
+bundle exec rake db:migrate app:db:test:prepare
+rspec spec/models spec/controllers
 result+=$?
+
+echo "*** Running teaser engine request specs"
+rspec spec/requests
+result+=$?
+
+echo "*** Running teaser engine javascript specs"
+bundle exec rake app:jasmine:ci
+result+=$?
+
+
+cd ../..
+cd gems/annoyance
+echo "*** Running annoyance gem specs"
+bundle install
+bundle exec rspec spec
+result+=$?
+
 
 exit $result
