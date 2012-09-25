bd8a6a8869e67785d71ef3fdc2bf7ed240f21ad9
Stephan Hagemann <stephan.hagemann@gmail.com>
2012-09-24 18:56:54 -0600

    Let the engine ensure that it's migrations are run on initialization.

--- engines/teaser/lib/teaser/engine.rb
+++ engines/teaser/lib/teaser/engine.rb
@@ -2,6 +2,8 @@ module Teaser
   class Engine < ::Rails::Engine
     isolate_namespace Teaser
 
-    config.paths['db/migrate'] << File.join(Teaser::Engine.paths['db/migrate'].first)
+    initializer :append_migrations do |app|
+      app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded unless app.root.to_s.match root.to_s
+    end
   end
 end
