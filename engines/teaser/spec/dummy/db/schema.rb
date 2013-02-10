f3ca9b747744280ea88b9cfd51edaa2c37490d49
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-02-10 11:49:22 -0700

    Updated schema files

--- engines/teaser/spec/dummy/db/schema.rb
+++ engines/teaser/spec/dummy/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121022034050) do
+ActiveRecord::Schema.define(:version => 20120917081547) do
 
-  create_table "news_signup_entries", :force => true do |t|
+  create_table "teaser_entries", :force => true do |t|
     t.string   "email"
-    t.integer  "tries",      :default => 0
     t.datetime "created_at",                :null => false
     t.datetime "updated_at",                :null => false
+    t.integer  "tries",      :default => 0
   end
 
 end
