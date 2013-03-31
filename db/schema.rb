2be070e3f0e1364d8bf37c1c90c8e5dec38b34bb
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-03-31 07:31:18 -0600

    Move exisiting entries from teaser entries to news signup entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130331124429) do
+ActiveRecord::Schema.define(:version => 20130331132356) do
 
   create_table "news_signup_entries", :force => true do |t|
     t.string   "email"
