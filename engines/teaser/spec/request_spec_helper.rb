6b5348b9a09e8360d1fd30d508156e42da931d52
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-03-31 06:25:39 -0600

    Explicitly include capybara DSL

--- engines/teaser/spec/request_spec_helper.rb
+++ engines/teaser/spec/request_spec_helper.rb
@@ -15,4 +15,5 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
   config.include RequestSpecHelpers
+  config.include Capybara::DSL
 end
