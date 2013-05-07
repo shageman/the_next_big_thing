a283ca08596fb1fa806c3f91cfe28ae40685becf
Stephan Hagemann <stephan.hagemann@gmail.com>
2013-05-06 22:41:47 -0600

    Make gem work with and without rails

--- gems/annoyance/lib/annoyance.rb
+++ gems/annoyance/lib/annoyance.rb
@@ -1,4 +1,9 @@
-require 'annoyance/engine' if defined?(Rails)
+if defined?(Rails)
+  require 'annoyance/engine'
+else
+  require_relative '../app/models/annoyance/levels'
+  require_relative '../app/models/annoyance/meter'
+end
 
 module Annoyance
 end
