c33f52a25ab6e64fff3c7418b1281f36f495ed32
Stephan Hagemann <stephan.hagemann@gmail.com>
2014-04-22 19:42:12 -0500

    Going back to nested folder structure

--- migrate_and_prepare_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ -s "$HOME/.rvm/scripts/rvm" ] && . "$HOME/.rvm/scripts/rvm"
-
-echo "migrating teaser"
-cd engines/teaser && rake db:migrate app:db:test:prepare && cd ../..
-echo "migrating wrapper rails app"
-rake db:migrate db:test:prepare
