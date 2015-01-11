0ad5cb868e98e42321a249190d0cedc0d3eaf6b8
Stephan Hagemann <stephan.hagemann@gmail.com>
2015-01-11 15:40:03 -0500

    More robust matcher to identify when engine is loaded by dummy app to prevent duplicate migrations

--- components/email_signup/lib/email_signup/engine.rb
+++ components/email_signup/lib/email_signup/engine.rb
@@ -3,7 +3,7 @@ module EmailSignup
     isolate_namespace EmailSignup
 
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
+      unless app.root.to_s.match root.to_s + File::SEPARATOR
         config.paths["db/migrate"].expanded.each do |path|
           app.config.paths["db/migrate"] << path
         end
